c8e770969b6f0d3ff8c818696009e456ef9df086
hoonv <chaeefe@gmail.com>
2020-12-24 13:18:42 +0900

    change Person scheme
    - 스킴변경

--- CoreDataStudy/CoreDataStudy/Model/Person.swift
+++ CoreDataStudy/CoreDataStudy/Model/Person.swift
@@ -2,7 +2,7 @@
 //  Person+CoreDataClass.swift
 //  CoreDataStudy
 //
-//  Created by 채훈기 on 2020/12/23.
+//  Created by 채훈기 on 2020/12/24.
 //
 //
 
@@ -11,12 +11,17 @@ import CoreData
 
 @objc(Person)
 public class Person: NSManagedObject {
-    
+
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Person> {
         return NSFetchRequest<Person>(entityName: "Person")
     }
 
-    @NSManaged public var name: String?
+    @NSManaged public var index: Int32
+    @NSManaged public var id: String?
+    @NSManaged public var guid: String?
+    @NSManaged public var isActive: Bool
+    @NSManaged public var balance: String?
+    @NSManaged public var picture: String?
     @NSManaged public var age: Int16
 
 }
